bacd83dab3b26c9ef4f1e6f8cc274db30fc688fe
agungxpp <palpalehot69@gmail.com>
2026-06-19 00:29:34 +0700

    Refactor loading mechanism in main.lua

--- main.lua
+++ main.lua
@@ -9,7 +9,6 @@ loader:SetProgress(0.2)
 local GUI = loadstring(game:HttpGet(repo.."utils/GUI.lua"))()
 loader:SetProgress(0.4)
 local modules = {
-    ngapung = loadstring(game:HttpGet(repo.."modules/ngapung.lua"))(),
     ngabret = loadstring(game:HttpGet(repo.."modules/ngabret.lua"))(),
     esp = loadstring(game:HttpGet(repo.."modules/esp.lua"))()
 }
